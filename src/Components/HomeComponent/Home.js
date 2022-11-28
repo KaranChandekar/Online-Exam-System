93f48af61d19865cf936b8fde3d71e70e1bab86c
Karan <karanchandekar431@gmail.com>
2022-11-28 14:26:27 +0530

    styled home section

--- src/Components/HomeComponent/Home.js
+++ src/Components/HomeComponent/Home.js
@@ -1,53 +1,41 @@
+import style from "./Home.module.css";
+import pic1 from "../../images/1.png";
+import pic2 from "../../images/2.png";
+import pic3 from "../../images/3.jpg";
 
- import style from "./Home.module.css";
- import pic1 from "../../images/1.png";
- import pic2 from "../../images/2.png";
- import pic3 from "../../images/3.jpg";
+import { NavLink } from "react-router-dom";
 
- import {NavLink} from "react-router-dom";
+function Home() {
+  return (
+    <>
+      <div id={style.header}>
+        <h3>Online Exam System</h3>
+      </div>
 
+      <div id={style.main}>
+        <div id={style.div1}>
+          <img src={pic1} alt="" />
+          <span>Online Exam</span>
+        </div>
 
-    
-    function Home(){
-        return(
-            <>
-               <div id={style.header}>
-                   <div id={style.headerHeadingBox}>
-                        <h3>Online Exam System</h3> 
-                    </div>
-                </div>
+        <div id={style.div2}>
+          <div className={style.div3}>
+            <NavLink exact to="/StudentLogin">
+              <img src={pic2} alt="" />
+              <span>Student</span>
+            </NavLink>
+          </div>
 
-              <div id={style.div1}>
-                  <img src={pic1} alt="" />
-                  <span>Online Exam</span>
-              </div>
+          <div className={style.div3}>
+            <NavLink to="/AdminLogin">
+              <img src={pic3} alt="" />
+              <span>Admin</span>
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
-
-              <div id={style.div2}>
-            
-                  <div className ={style.div3}>
-                     <NavLink exact  to="/StudentLogin">
-                        <img src={pic2} alt="" />
-                        <span>Student</span>
-                     </NavLink>
-                  </div>
-
-                  <div  className ={style.div3}>
-                    <NavLink  to="/AdminLogin">
-                       <img src={pic3} alt="" />
-                       <span>Admin</span>
-                     </NavLink> 
-                  </div>
-                
-              </div>
-
-
-             
-
-            </>
-        );
-    }
-
-     
-
-    export default Home;
+export default Home;
